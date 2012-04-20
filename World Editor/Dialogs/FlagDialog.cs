0f8bb9df6d122177a240e152c11abe52dcfbae83
Jivy <jivythejivy@gmail.com>
2012-04-20 13:03:30 +0200

    Amélioration du FlagDialog : ° Les noms sont affichés dans la liste au lieu des valeurs ° La sauvegarde est bien effectuée sous forme de bitmask ° Opti code

--- World Editor/Dialogs/FlagDialog.cs
+++ World Editor/Dialogs/FlagDialog.cs
@@ -11,13 +11,25 @@ namespace World_Editor.Dialogs
 {
     public partial class FlagDialog : Form
     {
-        public uint bitmask { get; set; }
+        public int bitmask { get; set; }
+
+        /*
+         * Pour fonctioner, l'enum devra être sous cette forme :
+         * enum NomEnum 
+         * {
+         *      FLAG_1  =   1,
+         *      FLAG_2  =   2,
+         *      FLAG_3  =   4,
+         *      FLAG_4  =   8,
+         *      etc...
+         * };
+         */
         public Type type { get; set; }
 
         public FlagDialog(Type flagType, uint loaded_bitmask = 0)
         {
             InitializeComponent();
-            bitmask = loaded_bitmask;
+            bitmask = (int)loaded_bitmask;
             type = flagType;
         }
 
@@ -25,36 +37,34 @@ namespace World_Editor.Dialogs
         {
             lstFlags.Items.Clear();
 
-            Array flags = Enum.GetValues(type);
-            Array flagNames = Enum.GetNames(type);
-
-            int i = 0;
-
-            foreach(var f in flags)
+            foreach (string f in Enum.GetNames(type))
             {
-                lstFlags.Items.Add(flagNames.GetValue(i));
-                
-                int mask = (int)Math.Pow(2, (double)((int)flags.GetValue(i) - 1));
+                lstFlags.Items.Add(f);
+
+                int mask = (int)Enum.Parse(type, f);
 
                 if (((int)bitmask & mask) == mask)
-                  lstFlags.SetItemChecked(i, true);
-
-                i = i + 1;
+                    lstFlags.SetItemChecked(lstFlags.Items.Count - 1, true);
             }
         }
 
         private void btnOk_Click(object sender, EventArgs e)
         {
-            btnOk.DialogResult = DialogResult.OK;
-            
-            // Bitmask saving
-            Array flags = Enum.GetValues(type);
-            Array flagNames = Enum.GetNames(type);
+            this.DialogResult = DialogResult.OK;
+
+            bitmask = 0;
+
+            foreach (string f in lstFlags.CheckedItems)
+                bitmask |= (int)Enum.Parse(type, f);
+
+            this.Close();
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
-            btnCancel.DialogResult = DialogResult.Cancel;
+            this.DialogResult = DialogResult.Cancel;
+
+            this.Close();
         }
     }
 }
