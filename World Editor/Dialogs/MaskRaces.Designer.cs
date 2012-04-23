ac8e6f207ceb78d5d7cba0512772aedb900fb345
Jivy <jivythejivy@gmail.com>
2012-04-23 13:31:21 +0200

    [FactionsEditor] Ajout de Tooltips

--- World Editor/Dialogs/MaskRaces.Designer.cs
+++ World Editor/Dialogs/MaskRaces.Designer.cs
@@ -71,6 +71,7 @@
             this.Controls.Add(this.listRaces);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "MaskRaces";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Liste des races";
             this.Load += new System.EventHandler(this.MaskRaces_Load);
             this.ResumeLayout(false);
